b9e248921112c06c101f345f518c8fd791df8eea
Wojciech Borzecki <114352015+borzenx@users.noreply.github.com>
2024-02-11 13:56:02 +0100

    d
    d

--- pages/api/similarweb/[query].ts
+++ pages/api/similarweb/[query].ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import fetch from 'node-fetch';
 
 type ResponseData = {
-  message: string;
+  message?: string;
 };
 
 export default async function handler(
@@ -10,27 +10,33 @@ export default async function handler(
   res: NextApiResponse<ResponseData>
 ) {
   try {
-    const query = req.query.query as string; // Accessing directly, no need for destructuring
-    
+    const query = req.query.query as string;
+
     if (!query || typeof query !== 'string') {
       throw new Error('Invalid query parameter');
     }
 
     const apiUrl = `https://data.similarweb.com/api/v1/data?domain=${query}`;
-    console.log('API URL:', apiUrl); // Log the API URL for debugging
+    console.log('API URL:', apiUrl);
 
     const response = await fetch(apiUrl);
-    
+
     if (!response.ok) {
       const errorMessage = await response.text();
       console.error('Failed to fetch data:', errorMessage);
       throw new Error('Failed to fetch data');
     }
 
-    const data = await response.json();
-    res.json(data as ResponseData); // Type assertion here
+    const data: ResponseData = await response.json() as ResponseData;
+    console.log('Response data:', data);
+
+    const responseData: ResponseData = {
+      message: data?.message || 'No message found'
+    };
+
+    res.status(200).json(responseData);
   } catch (error) {
     console.error('Error fetching data:', error);
-    res.status(500).json({ message: 'Error fetching data' }); // Respond with an error message
+    res.status(500).json({ message: 'Error fetching data' });
   }
 }
